c8db9877eb40541e1901c30ed9a09bbeb109695e
Dmitry Buryakov <buryakovdima@yandex.ru>
2015-09-05 21:04:54 +0300

    функции

--- sys.py
+++ sys.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
-outputNum = ''
-sumNumber = 0
-
 print '\033[1;42m Welcome to SYS1010 \033[1;m'
+print
+print '\033[1;42m Current ver: 1.0.0 ~ Author JADEWIZARD \033[1;m'
 print '\033[1;32mSelect an action \033[1;m'
 
 print('1: 10->2')
@@ -11,11 +10,12 @@ print('3: Return code')
 print('3: Return code')
 print('4: Additional code')
 print('5: Reverse number')
+print('EXIT: For exit')
 action = input('~')
 
-if action == 1:
-    print '\033[1;32mEnter a number in the decimal system \033[1;m'
-    number = input('~')
+def toBin(number):
+    outputNum = ''
+    sumNumber = 0
 
     while number > 0:
         currentNum = number
@@ -28,17 +28,34 @@ if action == 1:
         else:
             outputNum = '0' + str(outputNum)
 
-print 'OUTPUT ~ \033[1;38m'+outputNum+' \033[1;m'
+    print 'OUTPUT ~ \033[1;38m'+outputNum+' \033[1;m'
 
-if action == 2:
-    print '\033[1;32mEnter a number in the binary system \033[1;m'
-    number = input('~')
+def toDec(number):
+    outputNum = 0
     current = len(str(number))
 
     for i in str(number):
         current = current-1
         mNumber = int(i) * (2**current)
         #print(str(i)+' * 2^'+str(current))
-        sumNumber = sumNumber + mNumber
+        outputNum = outputNum + mNumber
 
-print(sumNumber)
+    print 'OUTPUT ~ \033[1;38m'+str(outputNum)+' \033[1;m'
+
+
+if action == 1:
+    print '\033[1;32mEnter a number in the decimal system \033[1;m'
+
+    number = input('~')
+    toBin(number)
+
+
+if action == 2:
+    print '\033[1;32mEnter a number in the binary system \033[1;m'
+
+    number = input('~')
+    toDec(number)
+
+if action == 3:
+    print '\033[1;32mEnter a number in the decimal system \033[1;m'
+    number = input('~')
